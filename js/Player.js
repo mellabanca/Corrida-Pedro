08b4913106e626aaf6f466bc4389c84f4072130f
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-09-16 16:09:20 -0300

    16/09/2022

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,10 @@ class Player {
     this.index = null;
     this.positionX = 0;
     this.positionY = 0;
+    this.rank = 0;
+    this.fuel = 185;
+    this.life = 185;
+    this.score = 0;
   }
 
   addPlayer() {
@@ -18,7 +22,19 @@ class Player {
     database.ref(playerIndex).set({
       name: this.name,
       positionX: this.positionX,
-      positionY: this.positionY
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score,
+      life: this.life
+    });
+  }
+
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
     });
   }
 
@@ -35,6 +51,17 @@ class Player {
     });
   }
 
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).update({
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score,
+      life: this.life
+    });
+  }
+
   static getPlayersInfo() {
     var playerInfoRef = database.ref("players");
     playerInfoRef.on("value", data => {
@@ -42,11 +69,15 @@ class Player {
     });
   }
 
-  update(){
-    var playerIndex = "players/player" + this.index;
-    database.ref(playerIndex).update({
-      positionX: this.positionX,
-      positionY: this.positionY,
-    })
+  getCarsAtEnd() {
+    database.ref("carsAtEnd").on("value", data => {
+      this.rank = data.val();
+    });
+  }
+
+  static updateCarsAtEnd(rank) {
+    database.ref("/").update({
+      carsAtEnd: rank
+    });
   }
 }
